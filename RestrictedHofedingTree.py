b6b51f5767d23205d0b3800dcd1c4ef0fe8bd349
sina <11491287+sinaak@users.noreply.github.com>
2020-01-25 19:50:09 +0100

    First commit

---
+++ RestrictedHofedingTree.py
@@ -0,0 +1,69 @@
+
+from skmultiflow.trees import HoeffdingTree
+import itertools 
+import numpy as np
+class RHT:
+	def __init__(self, K=1, base_learner=HoeffdingTree()):
+		self.K = K 
+		self.base_learner = base_learner
+		self.firstTime = True
+		self.featureSets = list()
+		self.models = list()
+		self.numberOfFeatures = None
+		self.alreadySeenInstances = 0
+
+
+	def partial_fit(self, X, y, classes=None, sample_weight=None):
+
+		n_data, n_features = X.shape
+
+		if self.firstTime:
+			self.firstTime = False
+			if not n_features > self.K:
+				raise Exception('K cannot be bigger than the number of features')
+
+			featureNames = list(range(0,n_features))
+			self.featureSets = list(itertools.combinations(featureNames, self.K))
+			self.models = [self.base_learner] * len(self.featureSets)
+			self.numberOfFeatures = len(self.featureSets)
+
+			for i, tupleFeature in enumerate(self.featureSets):
+				tmpTrainX = X[ : , tupleFeature]
+				self.models[i].partial_fit(tmpTrainX,y)
+		
+
+		for x in X:
+			p = []
+			for i, tupleFeature in enumerate(self.featureSets):
+				tmpTrainx = np.take(x, tupleFeature)
+				tmpTrainx = tmpTrainx.reshape(1, tmpTrainx.shape[0])
+				p.append(self.models[i].predict_proba(tmpTrainx))
+
+			print(len(p))
+			break
+
+
+
+		for i, tupleFeature in enumerate(self.featureSets):
+			tmpTrainX = X[ : , tupleFeature]
+			self.models[i].partial_fit(tmpTrainX,y)
+	
+
+
+
+
+
+
+
+		self.alreadySeenInstances += n_data
+
+		print(self.alreadySeenInstances)
+
+	def predict(self,X):
+		N, D = X.shape
+
+		return np.zeros(N)
+
+
+
+
