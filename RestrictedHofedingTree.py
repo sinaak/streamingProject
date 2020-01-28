f67d379a514a932eeef0205ef0a86086925833b4
sina <11491287+sinaak@users.noreply.github.com>
2020-01-28 17:39:42 +0100

    fixed bug for update_parameters

--- RestrictedHofedingTree.py
+++ RestrictedHofedingTree.py
@@ -1,72 +1,62 @@
-
 from skmultiflow.trees import HoeffdingTree
-import itertools 
+import itertools
 import numpy as np
 from perceptron import Tree_Perceptron
+
+
 class RHT:
-	def __init__(self, K=1, base_learner=HoeffdingTree()):
-		self.K = K 
-		self.base_learner = base_learner
-		self.firstTime = True
-		self.featureSets = list()
-		self.models = list()
-		self.numberOfFeatures = None
-		self.alreadySeenInstances = 0
+    def __init__(self, K=1, base_learner=HoeffdingTree()):
+        self.K = K
+        self.base_learner = base_learner
+        self.firstTime = True
+        self.featureSets = list()
+        self.models = list()
+        self.numberOfFeatures = None
+        self.alreadySeenInstances = 0
 
+    def partial_fit(self, X, y, classes=None, sample_weight=None):
 
-	def partial_fit(self, X, y, classes=None, sample_weight=None):
+        n_data, n_features = X.shape
 
-		n_data, n_features = X.shape
+        if self.firstTime:
+            self.firstTime = False
+            if not n_features > self.K:
+                raise Exception('K cannot be bigger than the number of features')
 
-		if self.firstTime:
-			self.firstTime = False
-			if not n_features > self.K:
-				raise Exception('K cannot be bigger than the number of features')
+            featureNames = list(range(0, n_features))
+            self.featureSets = list(itertools.combinations(featureNames, self.K))
+            self.models = [self.base_learner] * len(self.featureSets)
+            self.numberOfFeatures = len(self.featureSets)
 
-			featureNames = list(range(0,n_features))
-			self.featureSets = list(itertools.combinations(featureNames, self.K))
-			self.models = [self.base_learner] * len(self.featureSets)
-			self.numberOfFeatures = len(self.featureSets)
+            for i, tupleFeature in enumerate(self.featureSets):
+                tmpTrainX = X[:, tupleFeature]
+                self.models[i].partial_fit(tmpTrainX, y)
 
-			for i, tupleFeature in enumerate(self.featureSets):
-				tmpTrainX = X[ : , tupleFeature]
-				self.models[i].partial_fit(tmpTrainX,y)
+            self.perceptron = Tree_Perceptron(n_class=2, n_tree=len(self.featureSets), n_feature=self.numberOfFeatures)
 
-			perseptron = Tree_Perceptron(n_class=2, n_tree=len(self.featureSets), n_feature=self.numberOfFeatures)
+        for i, x in enumerate(X):
+            p = []
+            choosenY = y[i]
+            for i, tupleFeature in enumerate(self.featureSets):
+                tmpTrainx = np.take(x, tupleFeature)
+                tmpTrainx = tmpTrainx.reshape(1, tmpTrainx.shape[0])
+                p.append(self.models[i].predict_proba(tmpTrainx))
 
-		for i, x in enumerate(X):
-			p = []
-			choosenY = y[i]
-			for i, tupleFeature in enumerate(self.featureSets):
-				tmpTrainx = np.take(x, tupleFeature)
-				tmpTrainx = tmpTrainx.reshape(1, tmpTrainx.shape[0])
-				p.append(self.models[i].predict_proba(tmpTrainx))
+            self.perceptron.update_parameters(x, choosenY, p)
+            break
 
-			perseptron.update_parameters(x,choosenY,p)
-			print(len(p))
-			print(p[0].shape)
-			break
+        for i, tupleFeature in enumerate(self.featureSets):
+            tmpTrainX = X[:, tupleFeature]
+            self.models[i].partial_fit(tmpTrainX, y)
 
+        self.alreadySeenInstances += n_data
 
-		for i, tupleFeature in enumerate(self.featureSets):
-			tmpTrainX = X[ : , tupleFeature]
-			self.models[i].partial_fit(tmpTrainX,y)
-	
+        print(self.alreadySeenInstances)
 
+    def predict(self, X):
+        N, D = X.shape
 
-
-
-
-
-
-		self.alreadySeenInstances += n_data
-
-		print(self.alreadySeenInstances)
-
-	def predict(self,X):
-		N, D = X.shape
-
-		return np.zeros(N)
+        return np.zeros(N)
 
 
 
--- perceptron.py
+++ perceptron.py
@@ -4,7 +4,6 @@ import numpy as np
 class Tree_Perceptron:
 
     def __init__(self, n_class, n_tree, n_feature):
-
         self.learning_rate = 0
         self.n_class = n_class
         self.n_feature = n_feature
@@ -13,34 +12,50 @@ class Tree_Perceptron:
         self.instance_counter = 0
 
 
-    def update_parameters(self, X, y, Prob_List): # y should be the class number (zero base??)
+
+    def update_parameters(self, X, y, Prob_List):
+        print('instance arrived')
+        # update Learning Rate
+        self.instance_counter += 1
+        self.learning_rate = 2 / (2 + self.instance_counter + self.n_feature)
+
         for i in range(self.n_class):
             ai = self.get_ai(Prob_List, i)
             yp = 0
-            if y == i: #This class was the correct class
-                yp = 1
-            self.run_ai(ai, i, X, yp)
+            if y == i: yp = 1 # this class was the correct class
+            self.run_ai(ai, i, yp)
 
-        # Update Learning Rate
-        self.instance_counter += 1
-        self.learning_rate = 2 / (2 + self.instance_counter + self.n_feature)
+
+
+
 
     def get_ai(self, Prob_List, class_i):
         ci = []
         ai = []
-
         for P in Prob_List:
-            cij = P[class_i]
-            ci = ci.append(cij)
-            ai = ai.append( math.log(cij / (1 - cij)) )
+            cij = P.item(class_i)
+            ci.append(cij)
+            ai.append( math.log(cij / (1 - cij)) )
+
         return ai
 
-    def run_ai(self, ai, class_i, X, yp):
+
+
+    def run_ai(self, ai, class_i, yp):
         # update parameters
-        theta = np.zeros(self.n_tree)
-        for j in range(self.n_tree):
+        theta = np.zeros(self.n_tree+1)
+
+        for j in range(self.n_tree+1):
             theta[j] = self.parameters[class_i][j]
-        theta, cost = self.logistic_regression(X, yp, self.learning_rate, 3, theta=None)
+
+        print('lr: {}'.format(self.learning_rate))
+        theta, cost = self.logistic_regression(np.array([ai]), yp, self.learning_rate, 3, theta)
+
+        # for j in range(self.n_tree+1):
+        #     self.parameters[class_i][j] = theta[j]
+
+        pass
+
 
     def hypothesis(self, theta, X, n):
         h = np.ones((X.shape[0], 1))
@@ -50,6 +65,7 @@ class Tree_Perceptron:
         h = h.reshape(X.shape[0])
         return h
 
+
     def GD(self, theta, alpha, num_iters, h, X, y, n):
         cost = np.ones(num_iters)
         for i in range(0, num_iters):
@@ -57,24 +73,25 @@ class Tree_Perceptron:
             for j in range(1, n + 1):
                 theta[j] = theta[j] - (alpha / X.shape[0]) * sum((h - y) * X.transpose()[j])
             h = self.hypothesis(theta, X, n)
-            cost[i] = (-1 / X.shape[0]) * sum(y * np.log(h) + (np.ones(len(y)) - y) * np.log(1 - h))
+            cost[i] = (-1 / X.shape[0]) * sum(y * np.log(h) + (np.ones(len([y])) - y) * np.log(1 - h))
         theta = theta.reshape(1, n + 1)
         return theta, cost
+
 
     def logistic_regression(self, X, y, alpha, num_iters, theta=None):
         n = X.shape[1]
         if theta.all() == None:
             theta = np.zeros(n + 1)
-
         one_column = np.ones((X.shape[0], 1))
         X = np.concatenate((one_column, X), axis=1)
         h = self.hypothesis(theta, X, n)
         theta, cost = self.GD(theta, alpha, num_iters, h, X, y, n)
         return theta, cost
 
-    def run_perceptron(self, ai, class_i):
 
-        theta = np.zeros(self.n_tree)
+
+    def run_perceptron(self, ai, class_i):
+        theta =  np.zeros(self.n_tree)
         for j in range(self.n_tree):
             theta[j] = self.parameters[class_i][j]
 
@@ -82,7 +99,9 @@ class Tree_Perceptron:
 
         return prob
 
-    def predict_proba(self, X, Prob_List):
+
+
+    def predict_proba(self, Prob_List):
 
         P = [0 for x in range(self.n_class)]
 
@@ -97,5 +116,5 @@ class Tree_Perceptron:
 
     def make_param_table(self, n_class, n_tree):
         #make a big table to keep the learning theta
-        param_table = [[(1/n_class) for x in range(n_tree)] for y in range(n_class)]
+        param_table = [[(1/n_class) for x in range(n_tree+1)] for y in range(n_class)]
         return param_table
